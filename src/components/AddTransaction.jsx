b5c12704c55fc6f7897e8294797857f441e7c726
Jawwad-codes <jawwadnadeem78@gmail.com>
2024-09-13 20:47:19 -0700

    add category

--- src/components/AddTransaction.jsx
+++ src/components/AddTransaction.jsx
@@ -4,7 +4,45 @@ import { Globalcontext } from "../context/GlobalContext";
 export const AddTransaction = () => {
   const [text, setText] = useState("");
   const [amount, setAmount] = useState(0);
+  const [isactive, setActive] = useState(false);
   const { addTransaction } = useContext(Globalcontext);
+  const category = [
+    "Budget",
+    "gas",
+    "Electricity",
+    "Groceries",
+    "Internet",
+    "Rent",
+    "Water Bill",
+    "Phone Bill",
+    "Car Insurance",
+    "Health Insurance",
+    "Eating Out",
+    "Utilities",
+    "Entertainment",
+    "Transportation",
+    "Other",
+  ];
+  const dropdownStyles = {
+    color: "white",
+    padding: "10px",
+    borderRadius: "5px",
+    border: "1px solid gray",
+    width: "100%",
+    fontSize: "16px",
+  };
+  const handleAmountChange = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const handleIncomeClick = () => {
+    setActive(true);
+    setAmount(Math.abs(amount));
+  };
+  const handleExpenseClick = () => {
+    setActive(false);
+    setAmount(-Math.abs(amount));
+  };
   const onSubmit = (e) => {
     e.preventDefault();
     if (!text || !amount) {
@@ -17,20 +55,47 @@ export const AddTransaction = () => {
       amount: +amount,
     };
     addTransaction(newtransaction);
+    setAmount(0);
+    setText("");
   };
   return (
     <>
       <h3>Add new transaction</h3>
+      <div className="CheckButton">
+        <button
+          className={`Income-btn ${isactive ? "active" : ""}`}
+          onClick={handleIncomeClick}
+        >
+          Income
+        </button>
+        <button
+          className={`Expense-btn ${isactive ? "active" : ""}`}
+          onClick={handleExpenseClick}
+        >
+          Expence
+        </button>
+      </div>
       <form onSubmit={onSubmit}>
         <div className="form-control">
-          <label htmlFor="text">Text</label>
-          <input
-            type="text"
+          <label htmlFor="text">Expense Category</label>
+          <br />
+          <select
+            id="category"
+            name="category"
+            style={dropdownStyles}
             required
             value={text}
             onChange={(e) => setText(e.target.value)}
-            placeholder="Enter text..."
-          />
+          >
+            <option value="" disabled>
+              select a category
+            </option>
+            {category.map((category, index) => (
+              <option key={index} value={category}>
+                {category}
+              </option>
+            ))}
+          </select>
         </div>
         <div className="form-control">
           <label htmlFor="amount">
@@ -41,10 +106,11 @@ export const AddTransaction = () => {
             type="number"
             required
             value={amount}
-            onChange={(e) => setAmount(e.target.value)}
+            onChange={handleAmountChange}
             placeholder="Enter amount..."
           />
         </div>
+        <p>Fill form and select Income or Expense</p>
         <button className="btn">Add transaction</button>
       </form>
     </>
